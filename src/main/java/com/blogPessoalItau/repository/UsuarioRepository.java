23165199a018800e3083b08ebdffb4f024b94825
Luciana Neves <luciananbrito22@gmail.com>
2023-02-23 15:09:51 -0300

    "adicionando security e metodos de segurança e implementação de usuário"

--- src/main/java/com/blogPessoalItau/repository/UsuarioRepository.java
+++ src/main/java/com/blogPessoalItau/repository/UsuarioRepository.java
@@ -2,10 +2,12 @@ package com.blogPessoalItau.repository;
 
 import com.blogPessoalItau.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.crypto.spec.OAEPParameterSpec;
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     //
     //findByUsuario (sao palavras reservadas) -> procure pelo nome do atributo usuario
